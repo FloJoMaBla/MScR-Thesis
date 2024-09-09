3d7857a5dd0b9e5276d43de1aa5f4f1c4094b624
FloJoMaBla <159956124+FloJoMaBla@users.noreply.github.com>
2024-09-09 12:28:22 +0100

    Update Habitat Selection p4 2018.R

--- Habitat Selection p4 2018.R
+++ Habitat Selection p4 2018.R
@@ -47,12 +47,12 @@ print(schoeners_d_values_2018)
 set.seed(123)
 
 # Function to perform a permutation test
-perm_test_2018 <- function(DF, n_permutations = 1000) {
-  observed_d <- schoeners_d(DF)
+perm_test_2018 <- function(df.s_2018, n_permutations = 1000) {
+  observed_d <- schoeners_d(df.s_2018)
   null_d <- numeric(n_permutations)
   
   for (i in 1:n_permutations) {
-    permuted <- DF %>%
+    permuted <- df.s_2018 %>%
       mutate(Wintering = sample(Wintering))
     null_d[i] <- schoeners_d(permuted)
   }
